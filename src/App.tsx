5875e373e717c6d413ff701e8014a7cb7f125774
Suimrah <50170590+Suimrah@users.noreply.github.com>
2024-10-19 12:12:27 -0400

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+// import "./App.css";
 import image from "./assets/ilu.jpg";
 
 function App() {
@@ -7,14 +7,23 @@ function App() {
 
   return (
     <>
-      <div>
-        <button onClick={() => setisClicked(!isclicked)}>press me</button>
-        {isclicked && (
-          <div className="bg-blue-600">
-            <img src={image} height={400} alt="ilu" />
-            <div>Lilicuda</div>
+      <div className="h-full w-1/2 mx-auto mt-24">
+        <div className="grid justify-center space-y-4">
+          <div className="flex align-middle justify-center">
+            <button
+              onClick={() => setisClicked(!isclicked)}
+              className="bg-blue-700 rounded-md p-2 text-white justify-center"
+            >
+              press me
+            </button>
           </div>
-        )}
+
+          {isclicked && (
+            <div className="flex align-middle justify-center">
+              <img src={image} className="h-96 rounded-lg" alt="ilu" />
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
